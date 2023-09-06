c406ddc9f73317e963a2dd757530b559a2790f78
Junaid4ever <110425866+Junaid4ever@users.noreply.github.com>
2023-09-07 00:06:36 +0530

    Update a.py

--- a.py
+++ a.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 
 
-
 proxylist = [
     "192.99.101.142:7497",
     "198.50.198.93:3128",
@@ -17,7 +16,7 @@ proxylist = [
     "20.84.57.125:3128",
     "172.104.13.32:7497",
     "172.104.14.65:7497",
-    "165.225.220.241:10605",
+   "165.225.220.241:10605",
     "165.225.208.84:10605",
     "165.225.39.90:10605",
     "165.225.208.243:10012",
@@ -44,7 +43,7 @@ proxylist = [
 ]
 
 warnings.filterwarnings('ignore')
-fake = Faker('en_IN')
+fake = Faker('en_US')
 MUTEX = threading.Lock()
 
 
@@ -52,32 +51,26 @@ def sync_print(text):
     with MUTEX:
         print(text)
 
+
+
 def get_driver(proxy):
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
     options = webdriver.ChromeOptions()
     options.headless = True
-#    options.binary_location = "/usr/lib/chromium-browser/chromedriver"
     options.add_argument(f'user-agent={user_agent}')
     options.add_experimental_option("detach", True)
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--no-sandbox')
-    options.add_argument("--disable-gpu")
     options.add_argument('--disable-dev-shm-usage')
-    #options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-certificate-errors')
     options.add_argument('--allow-running-insecure-content')
-    #options.add_argument("--disable-extensions")
+    options.add_argument("--disable-extensions")
     options.add_argument("--proxy-server='direct://'")
     options.add_argument("--proxy-bypass-list=*")
     options.add_argument("--start-maximized")
-    #options.add_argument("--use-fake-ui-for-media-stream")
-    #options.add_argument("--use-fake-device-for-media-stream")
-    options.add_experimental_option("prefs", { \
-    "profile.default_content_setting_values.media_stream_mic": 1, 
-    "profile.default_content_setting_values.media_stream_camera": 1
-  })
     if proxy is not None:
         options.add_argument(f"--proxy-server={proxy}")
-    driver = webdriver.Chrome("chromedriver" ,options=options)
+    driver = webdriver.Chrome('chromedriver', options=options)
     return driver
 
 
@@ -91,32 +84,19 @@ def start(name, proxy, user, wait_time):
     sync_print(f"{name} started!")
     driver = get_driver(proxy)
     driver.get(f'https://zoom.us/wc/join/'+meetingcode)
-    driver.execute_script("navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => { console.log(stream) }).catch(error => { console.log(error) });")
     time.sleep(3)
     inp = driver.find_element(By.ID, 'inputname')
     time.sleep(1)
     inp.send_keys(f"{user}")
+    btn2 = driver.find_element(By.ID, 'joinBtn')
+    btn2.click()
     time.sleep(2)
-
     inp2 = driver.find_element(By.ID, 'inputpasscode')
     time.sleep(1)
     inp2.send_keys(passcode)
     btn3 = driver.find_element(By.ID, 'joinBtn')
     time.sleep(1)
     btn3.click()
-    time.sleep(5)
-    btn3 = driver.find_element(By.ID, 'preview-audio-control-button').click()
-    
-    #WebDriverWait(driver, 10).until( ec.presence_of_element_located((By.XPATH,'//*[@id="preview-audio-control-button"]'))).click()
-    #time.sleep(3)
-    WebDriverWait(driver, 10).until( ec.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div[1]/button'))).click()
-    time.sleep(5)
-    try:
-      WebDriverWait(driver, 10).until( ec.presence_of_element_located((By.XPATH,'/html/body/div[14]/div/div/div/div[2]/div/div/button'))).click()
-    except:
-      pass
-      
-    WebDriverWait(driver, 10).until( ec.presence_of_element_located((By.XPATH,'//*[@id="voip-tab"]/div/button'))).click()
     sync_print(f"{name} sleep for {wait_time} seconds ...")
     time.sleep(wait_time)
     sync_print(f"{name} ended!")
@@ -128,7 +108,7 @@ def main():
     for i in range(number):
         try:
             proxy = proxylist[i]
-        except Exception:
+        except IndexError:
             proxy = None
         try:
             user = fake.name()
